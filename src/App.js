65cd703c68130099654da1c1cb7bd4c37096a30a
Arup Mapa <arupmapa@gmail.com>
2024-08-06 02:04:48 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
- import { News } from "./Components/News";
+import { News } from "./Components/News";
 import { Navbar } from "./Components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
      <>
-     <Navbar/>
-     <News category="business"/>
+     <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<News key="general" category="general"/>}/>
+        <Route exact path="/business" element={<News key="business" category="business"/>}/>
+        <Route exact path="/entertainment" element={<News key="entertainment" category="entertainment"/>}/>
+        <Route exact path="/health" element={<News key="health" category="health"/>}/>
+        <Route exact path="/science" element={<News key="science" category="science"/>}/>
+        <Route exact path="/sports" element={<News key="sports" category="sports"/>}/>
+        <Route exact path="/technology" element={<News key="technology" category="technology"/>}/>
+      </Routes>
+     </BrowserRouter>
      </>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,25 +1,37 @@
 import React, { Component } from "react";
-
+import {Link} from "react-router-dom"
 export class Navbar extends Component {
     render() {
         return (
             <>
                 <nav className="navbar navbar-expand-lg bg-body-tertiary">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="/">NewsMonkey</a>
+                        <Link className="navbar-brand" to="/">NewsMonkey</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarNav">
                             <ul className="navbar-nav">
                                 <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/">Features</a>
+                                    <Link className="nav-link" to="/business">Business</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/">Pricing</a>
+                                    <Link className="nav-link" to="/entertainment">Entertainment</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/health">Health</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/science">Science</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/sports">Sports</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/technology">Technology</Link>
                                 </li>
                             </ul>
                         </div>
